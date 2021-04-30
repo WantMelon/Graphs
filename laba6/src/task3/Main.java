4ec0677a94dca0aeb011684d7767a91a34de4462
Саморосенко Артем Вячеславович <1901290086@ad.vsu>
2021-04-30 10:48:12 +0300

    Update task3

--- laba6/src/task3/Main.java
+++ laba6/src/task3/Main.java
@@ -1,4 +1,51 @@
 package task3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static int getNextVertex(int[][] matrix, boolean[] used, int v) {
+        for (int i = 0; i < matrix.length; ++i) {
+            if (!used[i] && matrix[v][i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Используем поиск в глубину для составления бинарного кода.
+     * @param matrix Исходная матрицы
+     * @param used Массив вершин, в которых мы уже побывали
+     * @param cur Текущая вершина
+     * @return Бинарный код
+     */
+    public static List<Integer> getBinaryCode(List<Integer> binaryCode,
+                                              int[][] matrix, boolean[] used,
+                                              int cur) {
+        used[cur] = true;
+        int next = getNextVertex(matrix, used, cur);
+
+        while (next != -1) {
+            binaryCode.add(1);
+            getBinaryCode(binaryCode, matrix, used, next);
+            binaryCode.add(0);
+            next = getNextVertex(matrix, used, cur);
+        }
+
+        return binaryCode;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = task1.Main.inputMatrix("src/task3/input.txt");
+        System.out.print("Root: ");
+        int root = new Scanner(System.in).nextInt();
+        boolean[] used = new boolean[matrix.length];
+        List<Integer> binaryCode = new ArrayList<>();
+        getBinaryCode(binaryCode, matrix, used, root);
+
+        System.out.println(binaryCode);
+    }
 }
